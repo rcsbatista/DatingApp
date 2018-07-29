117c25a71cded6cb679199420e218b459e6dc6bd
Ricardo Silva <rcs.batista@gmail.com>
2018-07-29 21:59:43 +0100

    Initial commit

---
+++ DatingApp.SPA/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent
+],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [
+    AuthService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
